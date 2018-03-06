152c8e1aba0629628d66cb114a0a6f9fbb02da12
Neil McEachin <njmceach@mtu.edu>
2018-03-06 12:50:21 -0500

    Created add articles page, submits form to mongo and added some bootstrap

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
 
 mongoose.connect('mongodb://localhost/expressTest');
 let db = mongoose.connection;
@@ -22,6 +25,11 @@ const port = 3000;
 let Article = require('./models/article');
 
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Load View Engine
 app.set('views', path.join(__dirname, 'views'));
@@ -49,7 +57,23 @@ app.get('/articles/add', (req, res) => {
 });
 
 
+// Add Submit POST Route
+app.post('/articles/add', (req,res) => {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save((err) => {
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect('/');
+        }
+    });
+});
+
 //Start Server
 app.listen(port, () => {
     console.log('Server started on port ' + port);
-})
+});
